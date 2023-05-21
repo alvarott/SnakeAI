4c90414058daef1eb71af1efd42f8d0a2951fb39
Alvaro <alvarouk230@gmail.com>
2023-05-21 19:30:46 +0200

    Added SnakeGui class to the __init__ module

--- src/snake_ai/snake/snake_core/__init__.py
+++ src/snake_ai/snake/snake_core/__init__.py
@@ -1,4 +1,5 @@
 from snake_ai.snake.snake_core.snake_core import SnakeCore
 from snake_ai.snake.snake_core.snake_gui_core.snake_gui_factory import SnakeGUIFactory
+from snake_ai.snake.snake_core.snake_gui_core.snake_gui_abc import SnakeGUI
 
 
