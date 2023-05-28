895c5a6413cd6b8cdb57290bb5fc3d394ffa1f95
Alvaro <alvarouk230@gmail.com>
2023-05-28 22:43:29 +0200

    Added parent class to NNFunctionsFactory

--- src/snake_ai/neural/nn_functions.py
+++ src/snake_ai/neural/nn_functions.py
@@ -5,55 +5,17 @@
 # Date: 18/05/2023
 # Version: 0.0.1
 
-from inspect import getmembers, isfunction, isclass
+from snake_ai.function_factory_abc import FunctionFactory
 import math
 import numpy as np
-import types
 
 
-class NNFunctionFactory:
+class NNFunctionFactory(FunctionFactory):
     """
     This class is meant to return an initialization or activation function by providing its name to a getter method
     """
     def __init__(self):
-        self._functions = self._map_functions()
-
-    def _map_functions(self):
-        """
-        Maps all the classes implemented in the module
-        :return:
-        """
-        functions = {}
-        classes = []
-        # Map classes in the module
-        for name, obj in globals().items():
-            if isclass(obj) and obj.__module__ == __name__ and obj.__name__.startswith('_'):
-                classes.append(obj)
-
-        # Map functions of each class
-        for obj in classes:
-            members = getmembers(obj)
-            class_functions = {name.lower(): value for name, value in members if isfunction(value) and not name.startswith('_')}
-            functions[obj.__name__[1:].lower()] = class_functions
-
-        return functions
-
-    def get_function(self, type: str, func: str) -> types.FunctionType:
-        """
-        Produces a function instance
-        :param type: function type
-        :param func: function name
-        :return: function
-        """
-        if type in self._functions.keys():
-            if func in self._functions[type].keys():
-                return self._functions[type][func]
-            else:
-                raise ValueError(f"No implementation found for '{func}' of the type '{type}'. "
-                                 f"Functions available {list(self._functions[type].keys())}")
-        else:
-            raise ValueError(f"No implementation found for functions of the type '{type}'."
-                             f" Available types {list(self._functions.keys())}")
+        super().__init__(globals(), __name__)
 
 
 class _Activation:
