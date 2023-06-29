fcce4dbd9764aaeec8d270d6d0e79cc1c63ffd9b
Alvaro <alvarouk230@gmail.com>
2023-06-29 22:33:50 +0200

    Added GUI buttons base class

---
+++ src/snake_ai/gui/abc_button.py
@@ -0,0 +1,43 @@
+# This module contains the base class for buttons
+
+# Author: Álvaro Torralba
+# Date: 21/06/2023
+# Version: 0.0.1
+
+from abc import ABC, abstractmethod
+import customtkinter as ctk
+
+
+class ButtonABC(ABC,ctk.CTkButton):
+    """
+    Buttons base class extend the CTKButton class
+    """
+    def __init__(self, variable: ctk.Variable = None, mediator = None, top_level = None, **kwargs):
+        """
+        Constructor
+        :param variable: value holder if any
+        :param kwargs: configuration parameters for the base class CTkButton
+        """
+        super().__init__(**kwargs)
+        self._variable = variable
+        self.mediator = mediator
+        self.top = top_level
+        self.configure(command=self._click_event)
+
+    @property
+    def value(self):
+        return self._variable.get()
+
+    @value.setter
+    def value(self, new_value):
+        self._variable.set(new_value)
+
+    @abstractmethod
+    def _click_event(self, *args, **kwargs):
+        """
+        This method defines the behavior of the button when it is pressed
+        :param args: any positional parameter needed
+        :param kwargs: any key-value parameter needed
+        :return:
+        """
+        pass
