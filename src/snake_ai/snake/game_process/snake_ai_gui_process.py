e47703c4f949e31ecde761327b79e2881d4f791e
Alvaro <alvarouk230@gmail.com>
2023-06-29 20:47:39 +0200

    Modified SnakeAIGUI constructor

--- src/snake_ai/snake/game_process/snake_ai_gui_process.py
+++ src/snake_ai/snake/game_process/snake_ai_gui_process.py
@@ -10,6 +10,7 @@ from snake_ai.snake.game_process.snake_gui_process import SnakeGUIProcess
 from snake_ai.snake.game_process.snake_ai_abc_process import SnakeAIABC
 from snake_ai.neural.nn import NN
 
+
 class SnakeAIGUI(SnakeGUIProcess, SnakeAIABC):
     """
     Implements a graphical snake process controlled by a NN
@@ -21,15 +22,7 @@ class SnakeAIGUI(SnakeGUIProcess, SnakeAIABC):
         :param core: SnakeGUI name to be used as core
         :param dist_calculator: Distance calculator name
         :param show_path: rendering min path flag
-        :param input: input nodes to the NN
-        :param output: output nodes from the NN
-        :param hidden: list with the number of nodes of each hidden layer
-        :param output_init: output layer weights initialization method to be used
-        :param bias: flag to introduce or not bias vector
-        :param bias_init: bias initialization method to be used
-        :param hidden_init: hidden layers initialization method to be used
-        :param output_act: output layer activation function
-        :param hidden_act: hidden layers activation function
+        :param brain: NN that controls the snake
         """
         controller = AIController(input=1, output=1, hidden=[1], output_init='he', bias=False,
                                   bias_init='he', hidden_init='he', output_act='relu',
