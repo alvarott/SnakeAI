72596855444690039b2839128c725b9a47ea169a
Alvaro Torralba <alvarouk230@gmail.com>
2023-08-22 23:22:06 +0200

    Added new binary and real vision methods

--- src/snake_ai/snake/vision/grid_ops.py
+++ src/snake_ai/snake/vision/grid_ops.py
@@ -4,29 +4,36 @@
 # Date: 17/05/2023
 # Version: 0.0.1
 
-import numpy as np
-from snake_ai.snake.enums import GridDict, NNVision, Step
 from snake_ai.snake.vision.dist_factory import DistanceFactory
+from snake_ai.snake.enums import GridDict, Step
 from queue import PriorityQueue
+import numpy as np
 
 
 class GridOps:
     """
     This class provides the operations with in the grid where is displayed the snake board
     """
-    def __init__(self, dist_calc: str):
+    def __init__(self, vision: str):
         """
         Constructor
-        :param dist_calc: flag to indicate which method to be used for calculating distances
+        :param vision: type of vision identifier
         """
-        self._dist_calc = DistanceFactory().get_instance(dist_calc)
+        self._manhattan = DistanceFactory().get_instance('Manhattan')
+        if vision in ['binary', 'real']:
+            if vision == 'binary':
+                self.vision = self._binary_vision
+            else:
+                self.vision = self._real_vision
+        else:
+            raise ValueError(f'vision type: "{vision}" not supported')
 
     @staticmethod
     def _belong(dimensions: tuple[int, int], point: np.ndarray) -> bool:
         """
         Checks if a given point belong to and N x M grid
         :param dimensions: weight and height of the grid
-        :param point: celll
+        :param point: cell
         :return:
         """
         dim = np.array([dimensions[0]-1, dimensions[1]-1])
@@ -35,7 +42,7 @@ class GridOps:
         else:
             return False
 
-    def _axis_vision(self, matrix: np.ndarray, center: np.ndarray, x_step: int = 0, y_step: int = 0) -> np.ndarray:
+    def _axis_vision(self, matrix: np.ndarray, center: np.ndarray, x_step: int = 0, y_step: int = 0) -> list[float]:
         """
         Calculates the distance from a cell along a scroll to find an object, used to find the distance between
         the head of the snake and the rest of possible objects
@@ -48,28 +55,26 @@ class GridOps:
         """
         step = np.array([y_step, x_step])
         ref = center + step
-        collision = False
-        vision = []
+        vision = [0.0, 0.0]
+        steps = 1
+        found = False
         # Loop locking for collisions
         while GridOps._belong(matrix.shape, ref):
             value = matrix[ref[0], ref[1]]
-            if value in (GridDict.BODY.value, GridDict.APPLE.value):
-                collision = True
-                dist = self._dist_calc.dist(matrix.shape, center, ref)
-                vision = list(NNVision[GridDict(value).name].value)
-                vision.append(dist)
-                break
+            if value == GridDict.BODY.value and not found:
+                vision[1] = 1 / steps
+                ref = ref + step
+                found = True
             else:
                 ref = ref + step
+            steps += 1
         # If a position outside the grid is reached without finding a collision the distance to the wall is calculated
         # instead
-        if not collision:
-            dist = self._dist_calc.dist(matrix.shape, center, ref - step)
-            vision = list(NNVision.WALL.value)
-            vision.append(dist)
-        return np.array(vision)
+        else:
+            vision[0] = 1 / steps
+        return vision
 
-    def vision(self, matrix: np.ndarray) -> np.ndarray:
+    def _real_vision(self, matrix: np.ndarray) -> np.ndarray:
         """
         Uses the method bellow over 8 axes around the origin cell to check for collisions
         :param matrix: grid where to look at
@@ -77,15 +82,64 @@ class GridOps:
         """
         center_y, center_x = np.where(matrix == GridDict.HEAD.value)
         center = np.array([center_y[0], center_x[0]])
-        vision = np.empty(0)
+        vision_wall = []
+        vision_body = []
+        vision_apple = [0, 0, 0, 0]
         # Calculate te distances in all axes
-        for step_x in Step.X.value.values():
-            for step_y in Step.Y.value.values():
-                if step_x == 0 and step_y == 0:
-                    continue
-                else:
-                    vision = np.append(vision, self._axis_vision(matrix, center, step_x, step_y))
-        return vision
+        for step in [-1, 1]:
+            vision_y = self._axis_vision(matrix, center, 0, step)
+            vision_x = self._axis_vision(matrix, center, step, 0)
+            vision_wall.extend([vision_y[0], vision_x[0]])
+            vision_body.extend([vision_y[1], vision_x[1]])
+        # Distance to apple
+        apple_y, apple_x = np.where(matrix == GridDict.APPLE.value)
+        dist_y = apple_y - center_y
+        dist_x = apple_x - center_x
+        if dist_y > 0:
+            vision_apple[2] = 1 / dist_y[0]
+        else:
+            if dist_y != 0:
+                vision_apple[0] = 1 / abs(dist_y[0])
+        if dist_x > 0:
+            vision_apple[3] = 1 / dist_x[0]
+        else:
+            if dist_x != 0:
+                vision_apple[1] = 1 / abs(dist_x[0])
+        return np.array(vision_wall + vision_body + vision_apple)
+
+    def _binary_vision(self, matrix: np.ndarray) -> np.ndarray:
+        center_y, center_x = np.where(matrix == GridDict.HEAD.value)
+        center = np.array([center_y[0], center_x[0]])
+        wall_vision = [0, 0, 0, 0]
+        body_vision = [0, 0, 0, 0]
+        apple_vision = [0, 0, 0, 0]
+        neighbours = {}
+        axis_counter = 0
+        # Check for collisions around the head
+        for i in range(-1, 2, 2):
+            neighbours[axis_counter] = (center - np.array([i, 0]))
+            neighbours[axis_counter + 1] = (center - np.array([0, i]))
+            axis_counter += 2
+        for position, neighbour in neighbours.items():
+            if not GridOps._belong(matrix.shape, neighbour):
+                wall_vision[position] = 1
+            else:
+                value = matrix[neighbour[0], neighbour[1]]
+                if value == GridDict.BODY.value:
+                    body_vision[position] = 1
+        apple_y, apple_x = np.where(matrix == GridDict.APPLE.value)
+        for i in range(4):
+            if apple_y >= center_y:
+                if apple_y > center_y:
+                    apple_vision[0] = 1.0
+            else:
+                apple_vision[2] = 1.0
+            if apple_x >= center_x:
+                if apple_x > center_x:
+                    apple_vision[1] = 1.0
+            else:
+                apple_vision[3] = 1.0
+        return np.array(wall_vision + body_vision + apple_vision)
 
     @staticmethod
     def _graph(matrix: np.ndarray) -> dict[tuple[int, int], list[tuple[int, int]]]:
@@ -107,7 +161,8 @@ class GridOps:
                         neighbours.append((i, j + edges[w]))
                         neighbours.append((i + edges[w], j))
                     for neighbour in neighbours:
-                        if ((0 <= neighbour[0] <= max_cell[0]) and (0 <= neighbour[1] <= max_cell[1])) and matrix[neighbour] != 1:
+                        if ((0 <= neighbour[0] <= max_cell[0]) and (0 <= neighbour[1] <= max_cell[1]))\
+                                and matrix[neighbour] != 1:
                             valid_neighbours.append(neighbour)
                     graph.update([((i, j), list(valid_neighbours))])
                     neighbours.clear()
@@ -122,12 +177,11 @@ class GridOps:
         :param target_cell: target cell
         :return path: the path found between the two cells if exists
         """
-
         def upd_score(g_score: float, node: tuple[int, int], predecessor: tuple[int, int] = None):
             nonlocal path, processed, open_hash, f_scores, g_scores, open
             if g_score < g_scores[node]:
                 g_scores[node] = g_score
-                f_scores[node] = g_score + self._dist_calc.dist(grid_dim, np.array(start_cell), np.array(target_cell))
+                f_scores[node] = g_score + self._manhattan.dist(grid_dim, np.array(start_cell), np.array(target_cell))
                 if predecessor is not None:
                     path[node] = predecessor
                 if node not in open_hash:
@@ -144,26 +198,28 @@ class GridOps:
                 current = path[current]
             rev_path.reverse()
             return rev_path
-
-        graph = self._graph(matrix)
-        grid_dim = matrix.shape
-        path = {}
-        processed = -1
-        open = PriorityQueue()
-        open_hash = set()
-        f_scores = {node: float('inf') for node in graph.keys()}
-        g_scores = {node: float('inf') for node in graph.keys()}
-        # Insert starting node
-        upd_score(g_score=0, node=start_cell)
-        # Process neighbours
-        while not open.empty():
-            node = open.get()[2]
-            open_hash.remove(node)
-            tmp_score = g_scores[node] + 1
-            # Check if the target node was reached
-            if node == target_cell:
-                return path_reverse(path)
-            # Process all adjacent nodes
-            for neighbour in graph[node]:
-                upd_score(g_score=tmp_score, node=neighbour, predecessor=node)
+        try:
+            graph = self._graph(matrix)
+            grid_dim = matrix.shape
+            path = {}
+            processed = -1
+            open = PriorityQueue()
+            open_hash = set()
+            f_scores = {node: float('inf') for node in graph.keys()}
+            g_scores = {node: float('inf') for node in graph.keys()}
+            # Insert starting node
+            upd_score(g_score=0, node=start_cell)
+            # Process neighbours
+            while not open.empty():
+                node = open.get()[2]
+                open_hash.remove(node)
+                tmp_score = g_scores[node] + 1
+                # Check if the target node was reached
+                if node == target_cell:
+                    return path_reverse(path)
+                # Process all adjacent nodes
+                for neighbour in graph[node]:
+                    upd_score(g_score=tmp_score, node=neighbour, predecessor=node)
+        except Exception:
+            return []
         return []
