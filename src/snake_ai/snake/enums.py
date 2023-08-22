c83b2287cc4ac9890189c9c5fadbdba6e758085f
Alvaro Torralba <alvarouk230@gmail.com>
2023-08-22 23:18:58 +0200

    Changed default game sizes and speeds

--- src/snake_ai/snake/enums.py
+++ src/snake_ai/snake/enums.py
@@ -22,10 +22,10 @@ class NNDirection(Enum):
     """
     Enumerate class representing the directions as entries for the NN
     """
-    UP = [1, 0, 0, 0]
-    DOWN = [0, 1, 0, 0]
-    LEFT = [0, 0, 1, 0]
-    RIGHT = [0, 0, 0, 1]
+    UP = [0, 0, 1, 0]
+    DOWN = [1, 0, 0, 0]
+    LEFT = [0, 0, 0, 1]
+    RIGHT = [0, 1, 0, 0]
 
 
 class NNOutput(Enum):
@@ -71,4 +71,4 @@ class GameSpeed(Enum):
     """
     SLOW = 10
     NORMAL = 18
-    FAST = 23
+    FAST = 50
