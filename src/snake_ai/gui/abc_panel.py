fd1142de2dd3b87fea6f1d93b322fe56e65e301b
Alvaro Torralba <alvarouk230@gmail.com>
2023-08-23 22:06:41 +0200

    Added flip widgets functionality and added propety to access the panel variable value

--- src/snake_ai/gui/abc_panel.py
+++ src/snake_ai/gui/abc_panel.py
@@ -5,14 +5,20 @@
 # Version: 0.0.1
 
 from abc import ABC, abstractmethod
+import snake_ai.gui._data as data
 import customtkinter as ctk
 
 
-class PanelABC(ABC,ctk.CTkFrame):
+class PanelABC(ABC, ctk.CTkFrame):
     def __init__(self, variable: ctk.Variable, **kwargs):
         super().__init__(**kwargs)
+        self._color_data = data.Colors()
         self._variable = variable
         self._text = ctk.CTkFont(family="Console", size=12, weight="bold")
+
+    @property
+    def value_var(self):
+        return self._variable
 
     @property
     def value(self):
@@ -22,10 +28,23 @@ class PanelABC(ABC,ctk.CTkFrame):
     def value(self, new_value):
         self._variable.set(new_value)
 
-    def disable_panel(self):
-        for widget in self.winfo_children():
-            widget.configure(state="disabled")
-
     @abstractmethod
     def _set_layout(self):
         pass
+
+    def _flip_widgets(self, widgets, state):
+        for widget in widgets:
+            if state:
+                if isinstance(widget, ctk.CTkLabel):
+                    widget.configure(text_color=self._color_data.inactive_text)
+                else:
+                    if isinstance(widget, ctk.CTkEntry):
+                        widget.configure(text_color=self._color_data.inactive_text_entry)
+                    widget.configure(state='disabled')
+            else:
+                if isinstance(widget, ctk.CTkLabel):
+                    widget.configure(text_color=self._color_data.active_text)
+                else:
+                    if isinstance(widget, ctk.CTkEntry):
+                        widget.configure(text_color=self._color_data.active_text_entry)
+                    widget.configure(state='normal')
