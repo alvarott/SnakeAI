80fb29e4563749dab8fd0b32a6688ff4f7bebc79
Alvaro Torralba <alvarouk230@gmail.com>
2023-09-07 03:13:51 +0200

    Removed locks from IO class

--- src/snake_ai/IO.py
+++ src/snake_ai/IO.py
@@ -5,15 +5,12 @@
 # Version: 0.0.1
 
 from typing import Type, Any
-from threading import Lock
 import hashlib
 import pickle
 import os
 
 
 class IO:
-    lock = Lock()
-
     @staticmethod
     def _calculate_checksum(data: bytes):
         """
@@ -31,11 +28,11 @@ class IO:
         :param obj: object instance to be saved
         :return:
         """
-        with IO.lock:
-            with open(os.path.join(path, name), 'wb') as file:
-                data = pickle.dumps(obj)
-                checksum = IO._calculate_checksum(data)
-                pickle.dump((checksum, data), file)
+
+        with open(os.path.join(path, name), 'wb') as file:
+            data = pickle.dumps(obj)
+            checksum = IO._calculate_checksum(data)
+            pickle.dump((checksum, data), file)
         return 0
 
     @staticmethod
@@ -47,16 +44,15 @@ class IO:
         :return: object instance
         """
         path = os.path.normpath(file_path)
-        with IO.lock:
-            with open(path, 'rb') as file:
-                try:
-                    checksum, obj = pickle.load(file)
-                    recalculated_checksum = IO._calculate_checksum(obj)
-                    # If the files is corrupted
-                    if checksum != recalculated_checksum:
-                        return -1
-                except Exception:
-                    return -2
+        with open(path, 'rb') as file:
+            try:
+                checksum, obj = pickle.load(file)
+                recalculated_checksum = IO._calculate_checksum(obj)
+                # If the files is corrupted
+                if checksum != recalculated_checksum:
+                    return -1
+            except Exception:
+                return -2
             obj = pickle.loads(obj)
             if isinstance(obj, obj_class):
                 return obj
